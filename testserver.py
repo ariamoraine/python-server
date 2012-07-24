4c9189de31cfd8ba02f4f658bd666e7c6d67fd67
Aria Moraine <ariamoraine@gmail.com>
2012-07-24 11:39:18 -0400

    added chating

--- testserver.py
+++ testserver.py
@@ -1,9 +1,23 @@
 import socket
+from threading import Thread
 
 s = socket.socket()
 s.bind(('10.242.11.167', 1234))
+
 s.listen(5)
 c = s.accept()
 s = c[0]
-ms = s.recv(10)
-print ms
+def recv():
+    while True:
+        data = s.recv(1024)
+        if not data: sys.exit(0)
+        print data
+
+Thread(target=recv).start()
+
+while True:
+    data = raw_input('>>>> ')
+    if not data: break
+    s.send(data)
+
+s.close()
