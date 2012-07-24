26f99bee782fdfb85ba03812b34c4eca13e553e2
Aria Moraine <ariamoraine@gmail.com>
2012-07-24 18:12:45 -0400

    added multi client

--- testserver.py
+++ testserver.py
@@ -2,25 +2,69 @@ import socket
 import sys
 from threading import Thread
 
-s = socket.socket()
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 running = True
+clientsockets = []
 
-if 'server' in sys.argv:
-    s.bind(('10.242.11.167', 5674))
-    s.listen(5)
-    c = s.accept()
-    s = c[0]
-else:
+def server_main_loop():
+    serversocket = socket.socket()
+    serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    serversocket.bind(('10.242.11.167', 5674))
+    serversocket.listen(5)
+    try:
+        while True:
+            c = serversocket.accept()
+            s = c[0]
+            clientsockets.append(s)
+            print 'clientsocketlist', clientsockets
+            Thread(target=recv, args=(s,)).start()
+    except KeyboardInterrupt:
+        running = False
+        sys.exit()
+
+def client_main_loop():
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.connect(('10.242.11.167', 5674))
+    Thread(target=client_recv, args=(s,)).start()
 
-def recv():
+    while True:
+        text = raw_input('>>>> ')
+        if running == False:
+            sys.exit()
+        elif text == 'exit':
+            s.send(text)
+            s.close()
+            sys.exit()
+        else:
+            print "sending text"
+            s.send(text)
+
+def client_recv(s):
+    print "starting client"
+    global running
+    while True:
+        try:
+            text = s.recv(1024)
+            print text
+        except socket.error:
+            running = False
+            s.close()
+            sys.exit()
+
+
+def recv(s):
+    print "thread started"
     global running
     while True:
         try:
             data = s.recv(1024)
-            print data
+            print "got some data"
+            for sock in clientsockets:
+                print "sent some data"
+                sock.send(data)
         except socket.error:
+            running = False
             sys.exit()
         if data == 'exit':
             running = False
@@ -28,15 +72,11 @@ def recv():
             sys.exit()
 
 
-Thread(target=recv).start()
+if __name__ == '__main__':
 
-while True:
-    text = raw_input('>>>> ')
-    if running == False:
-        sys.exit()
-    elif text == 'exit':
-        s.send(text)
-        s.close()
-        sys.exit()
+
+    if 'server' in sys.argv:
+        server_main_loop()
+
     else:
-        s.send(text)
+        client_main_loop()
