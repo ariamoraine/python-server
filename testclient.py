bf312b011dbf4548652cb60ca73e5268a00c95e3
Martha Kelly <mk.girdler@gmail.com>
2012-07-24 11:29:40 -0400

    additions to client to allow chat to server

--- testclient.py
+++ testclient.py
@@ -1,5 +1,21 @@
-import socket 
+import socket
+import sys
+from threading import Thread
 
 s = socket.socket()
 s.connect (("10.242.11.167", 1234))
-s.send('testing')
+
+def recv():
+    while True:
+        data = s.recv(1024)
+        if not data: sys.exit(0)
+        print data
+
+Thread(target=recv).start()
+
+while True:
+    data = raw_input('>>>> ')
+    if not data: break
+    s.send(data)
+
+s.close()
